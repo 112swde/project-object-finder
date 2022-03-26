bfb450fc96f288722abe180e8f2245d05ec8b5e4
112swde <82498343+112swde@users.noreply.github.com>
2022-03-26 21:46:12 +0530

    Add files via upload

--- project 135-137 video surveillance/main.js
+++ project 135-137 video surveillance/main.js
@@ -1,5 +1,30 @@
+status = "";
+item = "";
+
+
 function setup()
 {
     canvas = createCanvas(380,380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Object detected";
+    item = document.getElementById("input").value;
+}
+
+function modelLoaded()
+{
+    console.log("MOdel Loaded !");
+    status = true;
+}
+
+function draw()
+{
+    image(video,0,0,380,380);
 }
